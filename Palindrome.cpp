888f6cd7ea2f9fe06a8dd6c0588047dbf2df7b45
nashibata <55288319+nashibata@users.noreply.github.com>
2019-09-25 09:10:11 -0700

    Add files via upload

---
+++ Palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+#include <stdio.h>
+#include <ctype.h>1
+
+using namespace std;
+//Nicholas Shibata, 25 September, 2019
+//Palindrome takes in an input, checks if it's a palindrome, and prints the answer
+int main() {
+  cout << "Welcome to Palindrome" << endl << "Type in a word, I'll tell you if its a palindrome" << endl;
+  char input[30];
+  cin.get(input, 30);
+  cin.get();
+  char inputNew[30];
+												    char inverse[30];
+												    //checking for letters comes from cplusplus forum
+												    for (i = 0; i < 30; i++) {
+												      if (isalpha(input[i]))												    }
+}

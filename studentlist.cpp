758899df33f4773371d12f6a6d6c9bf9f3b22776
nashibata <55288319+nashibata@users.noreply.github.com>
2019-10-09 09:04:57 -0700

    Add files via upload

---
+++ studentlist.cpp
@@ -0,0 +1,28 @@
+#include <isotream>
+
+using namespace std:
+
+struct Student {
+  char namef[20];
+  char namel[20];
+  int id;
+  float gpa;
+};
+
+int main() {
+  //creates space in struct
+  Student nick;
+  cin >> nick.namef;
+  cin >> nick.namel;
+  george.id = 420009;
+  george.gpa = 3.8;
+  cout << nick.namef << nick.namel << "ID:" << nick.id << "GPA:" << nick.gpa << endl;
+
+
+  //vector pointer to vector of struct pointers piece here
+
+
+
+
+
+}

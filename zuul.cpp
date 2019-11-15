47f1a7f8a5bef90effdc51b7bc8d95c7abbbb817
nashibata <55288319+nashibata@users.noreply.github.com>
2019-11-15 09:15:07 -0800

    Add files via upload

---
+++ zuul.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include "Room.h"
+
+using namespace std;
+
+const int SIZE = 15;
+Room* list[SIZE];
+vector<Room*> vect;
+
+void printVectorStrings(vector<Room*>);
+
+int main() {
+
+  //variable creation
+  //list[whatevernumber] = new Room(new char["whateverdescription"])
+  //
+
+
+
+
+  
+}
